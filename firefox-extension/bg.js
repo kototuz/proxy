ccd20504e9b777ac322e6a7b02806e63d30c1b0c
kototuz <ivann1812@gmail.com>
2025-06-17 16:06:00 +0300

    [firefox-extension] fix: pac script url reloading -> `runtime.onStartup`

--- firefox-extension/bg.js
+++ firefox-extension/bg.js
@@ -5,5 +5,14 @@ browser.storage.local.get("pacScript").then(data => {
             proxyType: "autoConfig",
             autoConfigUrl: URL.createObjectURL(new Blob([data.pacScript], { type: "application/x-ns-proxy-autoconfig" }))
         }
+browser.runtime.onStartup.addListener(() => {
+    browser.storage.local.get("pacScript").then(data => {
+        browser.proxy.settings.set({
+            scope: "regular",
+            value: {
+                proxyType: "autoConfig",
+                autoConfigUrl: URL.createObjectURL(new Blob([data.pacScript], { type: "application/x-ns-proxy-autoconfig" }))
+            }
+        });
     });
 });
